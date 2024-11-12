bc6f064b28c40280f0631ed3da5be58b548cb896
刘铖浩 <1058338573@qq.com>
2024-11-12 14:51:11 +0800

    Update .gitignore and remove .next from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/liuchenghao/Documents/京城一灯/代码/个人代码/nextjs/dome01/src/app/layout":["static/media/463dafcda517f24f-s.p.woff","static/media/4473ecc91f70f139-s.p.woff"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"42vdUoHda2EjF6TzM28BmHycAlh5DRhufJl7VYqDhu4=\"\n}"

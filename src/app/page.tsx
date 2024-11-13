69fe78822ad53cdd4a801f5392d8d6f47f894c36
刘铖浩 <1058338573@qq.com>
2024-11-13 16:37:34 +0800

    服务组件获取数据添加suspense组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
-import { use } from "react";
+import { Suspense, use } from "react";
 async function getMassage() {
   try {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL+"api");
+    const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "api");
     const data = await response.json();
     return data;
   } catch (e) {
     console.log(e);
   }
 }
-export default function Home() {
+const MassageCom = () => {
   const massage = use(getMassage());
   return (
     <div>
       <h2>这是Home页面</h2>
       <h1>massage:{massage}</h1>
     </div>
+  );
+};
+export default function Home() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MassageCom />
+    </Suspense>
   );
 }
